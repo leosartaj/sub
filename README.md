ffb80d5d6431c7370c6d87c89e2aa38c37d50d3e
Sartaj Singh <singhsartaj94@gmail.com>
2014-10-21 21:07:36 +0530

    barebone

---
+++ README.md
@@ -0,0 +1,2 @@
+# README
+Download subtitles quickly
---
+++ api.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python2
+
+##
+# 
+# https://github.com/leosartaj/
+#
+# Copyright (c) 2014 Sartaj Singh
+# Licensed under the MIT license.
+##
+
+from hashlib import md5
+import requests 
+import os
+
+def pDir():
+    """
+    gives the present working directory
+    """
+    return os.getcwd()
+
+def fileExists(fName, dire=pDir()):
+    """
+    Check if a file exists
+    """
+    if os.path.isfile(os.path.join(dire, fName)):
+        return True
+    return False
+
+def get_hash(fName, readSize, dire=pDir()):
+    """
+    creates the required hash
+    """
+    if not fileExists(fName, dire):
+        return -1
+    readSize = readSize * 1024 # bytes to be read
+    fName = os.path.join(dire, fName) # name coupled with path
+    with open(fName, 'rb') as f:
+        size = os.path.getsize(fName)
+        data = f.read(readSize)
+        f.seek(-readSize, os.SEEK_END)
+        data += f.read(readSize)
+    return md5(data).hexdigest() # return md5 hash
+#print get_hash('blacklists02e05.mp4', 64, '/home/leosartaj/Downloads/')
+
+def download_file(fName, dire=pDir()):
+    """
+    download the required subtitle
+    """
+    # hash
+    gen_hash = get_hash(fName, 64, dire)
+    if gen_hash == -1:
+        return -1
+
+    # making request
+    user_agent = {'User-agent': 'SubDB/1.0 (PySub/0.1; http://github.com/leosartaj/PySub)'}
+    param = {'action': 'download', 'hash': gen_hash, 'language': 'en'} # Specification for the request
+    r = requests.get("http://api.thesubdb.com/", headers = user_agent, params = param) # Get Request
+    if r.status_code != '200':
+        return r.status_code
+
+    # save file
+    fName += '.srt'
+    fName = os.path.join(dire, fName) # name coupled with path
+    with open(fName, 'wb') as f:
+        f.write(r.text.encode('ascii', 'ignore'))
+
+    return r.status_code 
+
+print download_file('blacklists02e05.mp4', '/home/leosartaj/Downloads')
